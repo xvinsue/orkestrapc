65d01ad6960418f5c9359e8c35ea5b23c11691a4
vinsu <vencepuno.work@gmail.com>
2024-03-20 16:05:14 +0800

    adjust all

--- main.py
+++ main.py
@@ -561,7 +561,7 @@ def login():
         ic(is_user_exists)
         if is_user_exists:
             login_user(is_user_exists, remember=True)
-            return redirect(url_for("view"))
+            return redirect(url_for("home"))
 
         else:
 
