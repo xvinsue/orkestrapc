0254833448e86f7eb3df0dcecf2451afebb9693e
vinsu <vencepuno.work@gmail.com>
2024-03-26 16:04:50 +0800

    Revert "Merge branch 'main' into feature/frontend"
    This reverts commit e4c829cd79cffc129d2c05a55805a6bcd8d78f3d, reversing
    changes made to 0509b6a5a78009b5617d278ecdae9a77c1e9a0c9.

--- form.py
+++ form.py
@@ -27,8 +27,7 @@ class UnassignedAgents(FlaskForm):
 
 
 class employeeForm(FlaskForm):
-    
-    id = IntegerField(label="id", validators=[DataRequired()])
+
     name = StringField(label="Agent full name", validators=[DataRequired(), Length(min=5)])
     role = SelectField(label='Category', choices=['MSE', 'SA', 'FSE', 'HR', 'Recruit', 'Intern'])
 
